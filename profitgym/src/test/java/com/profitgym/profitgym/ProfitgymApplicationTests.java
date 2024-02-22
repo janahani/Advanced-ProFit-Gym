af7a7b2229d060e3ac2e0c493d8edcb4bd1867ac
Yassmin Ezzat <yassmin2104022@miuegypt.edu.eg>
2024-02-22 03:57:55 +0200

    Initialized Spring Boot setup

---
+++ profitgym/src/test/java/com/profitgym/profitgym/ProfitgymApplicationTests.java
@@ -0,0 +1,13 @@
+package com.profitgym.profitgym;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProfitgymApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
