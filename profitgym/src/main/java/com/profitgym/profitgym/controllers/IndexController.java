4d161760ab734365b97ee8543c34d52c4e267375
Yassmin Ezzat <yassmin2104022@miuegypt.edu.eg>
2024-02-25 23:15:08 +0200

    Updated index controller

--- profitgym/src/main/java/com/profitgym/profitgym/controllers/IndexController.java
+++ profitgym/src/main/java/com/profitgym/profitgym/controllers/IndexController.java
@@ -1,13 +1,15 @@
 package com.profitgym.profitgym.controllers;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.ModelAndView;
 
 @RestController
+@RequestMapping("")
 public class IndexController {
     
-    @GetMapping("index")
+    @GetMapping("/index")
     public ModelAndView getIndex() {
         ModelAndView mav = new ModelAndView("index.html");
         return mav;
