88ee60252ffe4190119d7bce78554466dea55d85
janahani <Jana2100237@miuegypt.edu.eg>
2024-02-25 22:50:52 +0200

    logout and back to homepage

---
+++ profitgym/src/main/java/com/profitgym/profitgym/controllers/IndexController.java
@@ -0,0 +1,15 @@
+package com.profitgym.profitgym.controllers;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.ModelAndView;
+
+@RestController
+public class IndexController {
+    
+    @GetMapping("index")
+    public ModelAndView getIndex() {
+        ModelAndView mav = new ModelAndView("index.html");
+        return mav;
+    }
+}
