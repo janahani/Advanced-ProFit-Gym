aecf394f1b7d9bb846831acf7c7e096700369355
Yassmin Ezzat <yassmin2104022@miuegypt.edu.eg>
2024-03-06 02:12:50 +0200

    Update in registration page.

--- profitgym/src/main/java/com/profitgym/profitgym/repositories/ClientRepository.java
+++ profitgym/src/main/java/com/profitgym/profitgym/repositories/ClientRepository.java
@@ -4,6 +4,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import com.profitgym.profitgym.models.Client;
 
 public interface ClientRepository extends JpaRepository<Client,Integer>{
-
     
 }
