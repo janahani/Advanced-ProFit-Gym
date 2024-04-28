aa4c7192a4c4afb400e29abbcc51722de569c512
Yassmin Ezzat <yassmin2104022@miuegypt.edu.eg>
2024-04-28 15:34:42 +0300

    Added a slight change to classes repository.

--- profitgym/src/main/java/com/profitgym/profitgym/repositories/ClassesRepository.java
+++ profitgym/src/main/java/com/profitgym/profitgym/repositories/ClassesRepository.java
@@ -3,7 +3,9 @@ package com.profitgym.profitgym.repositories;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.profitgym.profitgym.models.Classes;
+import java.util.List;
+
 
 public interface ClassesRepository extends JpaRepository<Classes,Integer>{
-    
+    Classes findByID(int iD);
 }
