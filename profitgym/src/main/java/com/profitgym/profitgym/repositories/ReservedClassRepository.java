d22be8fb76d553e5da9282fd292a44c3d8ed4259
lailanabill <laila2100232@miuegypt.edu.eg>
2024-05-08 15:04:09 +0300

    add find by id

--- profitgym/src/main/java/com/profitgym/profitgym/repositories/ReservedClassRepository.java
+++ profitgym/src/main/java/com/profitgym/profitgym/repositories/ReservedClassRepository.java
@@ -2,9 +2,16 @@ package com.profitgym.profitgym.repositories;
 import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.profitgym.profitgym.models.Memberships;
 import com.profitgym.profitgym.models.ReservedClass;
 
 public interface ReservedClassRepository extends JpaRepository<ReservedClass,Integer>{
 
      List<ReservedClass> findByIsActivated(String isActivated);
+
+
+      ReservedClass findById(int ID); 
+
+
 }
