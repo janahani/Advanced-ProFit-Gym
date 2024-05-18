de7e0705cb9364db6a8aa706d685c1c10787f1cd
Yassmin Ezzat <yassmin2104022@miuegypt.edu.eg>
2024-05-18 14:37:35 +0300

    package microservices

---
+++ packagemicroservices/src/main/java/com/profitgym/packagemicroservice/packagemicroservices/PackagemicroservicesApplication.java
@@ -0,0 +1,13 @@
+package com.profitgym.packagemicroservice.packagemicroservices;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PackagemicroservicesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PackagemicroservicesApplication.class, args);
+	}
+
+}
---
+++ packagemicroservices/src/test/java/com/profitgym/packagemicroservice/packagemicroservices/PackagemicroservicesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.profitgym.packagemicroservice.packagemicroservices;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PackagemicroservicesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
