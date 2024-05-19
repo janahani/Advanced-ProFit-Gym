90dc679fb327f2b1d4ddaf1281e53e7148816173
malakHelmy <121347791+malakHelmy@users.noreply.github.com>
2024-05-19 20:13:30 +0300

    added membership microservices

---
+++ membershipmicroservices/src/main/java/com/profitgym/membershipmicroservices/membershipmicroservices/MembershipmicroservicesApplication.java
@@ -0,0 +1,13 @@
+package com.profitgym.membershipmicroservices.membershipmicroservices;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MembershipmicroservicesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MembershipmicroservicesApplication.class, args);
+	}
+
+}
---
+++ membershipmicroservices/src/test/java/com/profitgym/membershipmicroservices/membershipmicroservices/MembershipmicroservicesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.profitgym.membershipmicroservices.membershipmicroservices;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MembershipmicroservicesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
