960c0aa63b051ed42c02cdc589b10f98b88a69e4
malakHelmy <121347791+malakHelmy@users.noreply.github.com>
2024-05-27 19:28:36 +0300

    freeze membership client hhhhh

--- profitgym/src/main/java/com/profitgym/profitgym/repositories/ScheduledUnfreezeRepository.java
+++ profitgym/src/main/java/com/profitgym/profitgym/repositories/ScheduledUnfreezeRepository.java
@@ -1,11 +1,11 @@
-// package com.profitgym.profitgym.repositories;
-// import org.springframework.data.jpa.repository.JpaRepository;
+package com.profitgym.profitgym.repositories;
+import org.springframework.data.jpa.repository.JpaRepository;
 
-// import com.profitgym.profitgym.models.ScheduledUnfreeze;
-// import java.util.List;
+import com.profitgym.profitgym.models.ScheduledUnfreeze;
+import java.util.List;
 
 
-// public interface ScheduledUnfreezeRepository extends JpaRepository<ScheduledUnfreeze,Integer>{
-//     ScheduledUnfreeze findByMembershipID(int membershipID);
+public interface ScheduledUnfreezeRepository extends JpaRepository<ScheduledUnfreeze,Integer>{
+    ScheduledUnfreeze findByMembershipID(int membershipID);
     
-// }
+}
